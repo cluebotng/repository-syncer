ff05900943dde51851aa7240eb70402a078abbd6
Damian Zaremba <damian@damianzaremba.co.uk>
2025-11-01 18:47:04 +0100

    Download GitHub SSH key fingerprints

--- repository_syncer/cli.py
+++ repository_syncer/cli.py
@@ -6,7 +6,7 @@ import os
 import subprocess
 import tempfile
 from pathlib import PosixPath
-from typing import Optional, Dict
+from typing import Optional, Dict, List
 
 import click
 import requests
@@ -59,6 +59,27 @@ def _calculate_process_env() -> Dict[str, str]:
     return proc_env
 
 
+def _fetch_gitlab_known_hosts() -> List[str]:
+    logger.info("Downloading production known_hosts file")
+    r = requests.get(
+        "https://config-master.wikimedia.org/known_hosts",
+        headers={"User-Agent": "ClueBot Repository Syncer"},
+    )
+    r.raise_for_status()
+    return [line for line in r.text.splitlines() if not line.startswith("#")]
+
+
+def _fetch_github_known_hosts() -> List[str]:
+    logger.info("Downloading Github known_hosts file")
+    r = requests.get(
+        "https://api.github.com/meta",
+        headers={"User-Agent": "ClueBot Repository Syncer"},
+    )
+    r.raise_for_status()
+    data = r.json()
+    return [f"github.com {ssh_key}" for ssh_key in data["ssh_keys"]]
+
+
 def _setup_environment():
     # SSH
     ssh_dir = home_dir / ".ssh"
@@ -69,20 +90,15 @@ def _setup_environment():
 
     # SSH known hosts
     if not manage_environment:
-        logger.info(f"Skipping ssh known_hosts file due to manage_environment")
+        logger.info("Skipping ssh known_hosts file due to manage_environment")
     else:
-        logger.info("Downloading production known_hosts file")
-        r = requests.get(
-            "https://config-master.wikimedia.org/known_hosts",
-            headers={"User-Agent": "ClueBot Repository Syncer"},
-        )
-        r.raise_for_status()
+        ssh_known_hosts = _fetch_gitlab_known_hosts() + _fetch_github_known_hosts()
         with (ssh_dir / "known_hosts").open("w") as fh:
-            fh.write(r.text)
+            fh.write("\n".join(ssh_known_hosts))
 
     # SSH config
     if not manage_environment:
-        logger.info(f"Skipping ssh config entry due to manage_environment")
+        logger.info("Skipping ssh config entry due to manage_environment")
     else:
         logger.info("Creating config entry for gitlab")
         with (ssh_dir / "config").open("w") as fh:
@@ -93,7 +109,7 @@ def _setup_environment():
     # SSH key
     encoded_ssh_key = os.environ.get("GITLAB_SSH_KEY")
     if not encoded_ssh_key:
-        raise RuntimeError(f"Missing ssh-key, ensure GITLAB_SSH_KEY is set")
+        raise RuntimeError("Missing ssh-key, ensure GITLAB_SSH_KEY is set")
 
     if not ssh_auth_socket.is_socket():
         logger.info(f"Starting ssh-agent on {ssh_auth_socket.as_posix()}")
